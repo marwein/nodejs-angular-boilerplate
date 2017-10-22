21c5aa4ec4b3dc67b843db8e887748b0314a82b5
BOUALLOUS Marwein <mbouallo@student.42.fr>
2017-10-22 23:35:38 +0200

    make it better

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(helmet());
 
 app.set('view engine', 'html');
 app.engine('html', ejs.renderFile);
-app.set('views', path.join(__dirname, 'public'));
+app.set('views', path.join(__dirname, 'views'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
