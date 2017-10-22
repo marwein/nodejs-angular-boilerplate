a7332900214811abccf3561b0534db796b27b4cb
BOUALLOUS Marwein <mbouallo@student.42.fr>
2017-10-23 00:14:10 +0200

    delete extra files

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(helmet());
 
 app.set('view engine', 'html');
 app.engine('html', ejs.renderFile);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public'));
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -62,7 +62,7 @@ app.use(function(err, req, res, next) {
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.send('error');
 });
 
 module.exports = app;
