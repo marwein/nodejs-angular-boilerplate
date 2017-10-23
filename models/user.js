e8121c81b3adbe5846d9f8f439d08efa3ad75703
BOUALLOUS Marwein <mbouallo@student.42.fr>
2017-10-23 08:26:28 +0200

    adding Mongoose validators

--- models/user.js
+++ models/user.js
@@ -1,14 +1,14 @@
 var mongoose = require('mongoose');
+var validators = require('mongoose-validators');
 var Schema = mongoose.Schema;
 var mongooseUniqueValidator = require('mongoose-unique-validator');
-var validators = require('mongoose-validators');
 
 var schema = new Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    login: { type: String, required: true, unique: true },
+    login: { type: String, required: true, unique: true, validate: [validators.isAlphanumeric(), validators.isLength(8, 20)] },
     password: { type: String, required: true },
-    email: { type: String, required: true, unique: true }
+    email: { type: String, required: true, unique: true, validate: validators.isEmail() }
 });
 
 schema.plugin(mongooseUniqueValidator);
