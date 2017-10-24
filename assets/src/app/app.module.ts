9c94bc565fea1e8af569f13e0301887c9e6d94fa
BOUALLOUS Marwein <mbouallo@student.42.fr>
2017-10-24 06:07:16 +0200

    deploying

--- assets/src/app/app.module.ts
+++ assets/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { MarweinComponent } from './marwein/marwein.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarweinComponent
   ],
   imports: [
     NgbModule.forRoot(),
---
+++ assets/src/app/marwein/marwein.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marwein',
+  templateUrl: './marwein.component.html',
+  styleUrls: ['./marwein.component.css']
+})
+export class MarweinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
