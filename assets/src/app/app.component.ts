54deb114c4719b38664293950e639a1796fb18c7
BOUALLOUS Marwein <mbouallo@student.42.fr>
2017-10-25 16:38:23 +0200

    deploying

--- assets/src/app/app.component.ts
+++ assets/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Input, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
